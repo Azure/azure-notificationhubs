0b1e211863c4e79143f6c6998d7f83c16bc51f62
smithab <smithab@microsoft.com>
2016-05-11 15:58:47 -0700

    NH Github.io files

--- iOS/BVT/Headers/SdkTests/HubInstantiationTests.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "IosSdkTests.h"
-#import "TokenProvider.h"
-
-
-@interface HubInstantiationTests : IosSdkTests
-@end
--- iOS/BVT/Headers/SdkTests/SyncRegMgmtTests.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "IosSdkTests.h"
-
-@interface SyncRegMgmtTests : IosSdkTests
-
-@end
