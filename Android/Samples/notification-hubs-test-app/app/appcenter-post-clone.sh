a07095dcb981412bd8a3907afd7886a62454a605
Tomasz Kmiecik <tomaskmi@microsoft.com>
2018-10-25 16:09:45 -0700

    Updating output path in post-clone script

--- Android/Samples/notification-hubs-test-app/app/appcenter-post-clone.sh
+++ Android/Samples/notification-hubs-test-app/app/appcenter-post-clone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo $"GOOGLE_SERVICES_JSON" > google-services.json
+echo $GOOGLE_SERVICES_JSON > $APPCENTER_SOURCE_DIRECTORY/Android/Samples/notification-hubs-test-app/app/google-services.json
