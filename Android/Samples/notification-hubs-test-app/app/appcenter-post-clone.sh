b9a5c04679cfaf8c1af897904425a4ae2688720f
Tomasz Kmiecik <tomaskmi@microsoft.com>
2018-10-25 16:21:25 -0700

    Adding echo to post-clone script

--- Android/Samples/notification-hubs-test-app/app/appcenter-post-clone.sh
+++ Android/Samples/notification-hubs-test-app/app/appcenter-post-clone.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-echo $GOOGLE_SERVICES_JSON > $APPCENTER_SOURCE_DIRECTORY/Android/Samples/notification-hubs-test-app/app/google-services.json
+OUTPUT_PATH=$APPCENTER_SOURCE_DIRECTORY/Android/Samples/notification-hubs-test-app/app/google-services.json
+
+echo $GOOGLE_SERVICES_JSON > $OUTPUT_PATH
+
+echo "Writing google-services.json: $OUTPUT_PATH"
+echo "${OUTPUT_PATH:0:20}"
+
